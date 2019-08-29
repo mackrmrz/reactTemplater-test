b917ff569b3a37d1b57e6e2d7c304877e377631f
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2019-08-28 18:38:44 -0700

    First Commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Starting the react Project</h1>
+        <h2>React Bootstrap</h2>
+        <h1>Hello dr Nick</h1>
+      </div>
+    );
+  }
+}
