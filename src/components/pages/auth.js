d6c8502e328788bb4262a76271adb9d9bdc9b035
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2020-01-31 07:26:56 -0800

    capitalization

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Login from "../auth/login";
-import loginImg from "../../../static/assets/AllMyImages/images/login.jpg";
+import loginImg from "../../../static/assets/allMyImages/images/login.jpg";
 
 export default class Auth extends Component {
   constructor(props) {
